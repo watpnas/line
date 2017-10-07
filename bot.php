dd1c7b4e874bd1fa1e8d1c5fa141bb482bf0f673
watpnas <watpnas@gmail.com>
2017-10-07 15:39:48 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,7 +12,7 @@ if (!is_null($events['events'])&&false) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['source']['userId'];
+			$text = $event['source']['groupId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
