8e57c01fa4d3452d57cc435babc5c7305d49a90d
watpnas <watpnas@gmail.com>
2017-10-10 17:29:55 +0700

    go

--- Check-in.php
+++ Check-in.php
@@ -1,18 +1,3 @@
-<?php
-$servername = "localhost";
-$username = "root@localhost";
-$password = "";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
-?>
-
 <html>
 <body>
 
