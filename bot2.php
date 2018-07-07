ace289eeb068300bb3b2472fa576f3e86cc80a7b
watpnas <watpnas@gmail.com>
2018-07-07 23:46:55 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -3,7 +3,6 @@
 $content = file_get_contents('php://input');
 // Parse JSON
 
-$url = 'https://api.line.me/oauth2/v2.1/token';
 $data = array( 'grant_type' => 'authorization_code', 'code' => $_GET["code"], 'redirect_uri' => 'https://dixellcallback.herokuapp.com/bot2.php', 'client_id' => '1592580012', 'client_secret' => '597224391a23fc9efb7a9646cd1f016d');
 
 // use key 'http' even if you send the request to https://...
@@ -16,15 +15,16 @@ $options = array(
 );
 echo "login\n";
 foreach ($data as $d) {
-    echo $d->type; 
+    echo $d; 
     echo "<br/>";
 } 
 
 
+$url = 'https://api.line.me/oauth2/v2.1/token';
 $context  = stream_context_create($options);
 $result = file_get_contents($url, false, $context);
 echo "get token\n";
-if ($result === FALSE) { /* Handle error */   echo 'result error'; }
+if ($result === FALSE) { /* Handle error */   echo "result error"; }
 
 echo 'content='.$content;
 
