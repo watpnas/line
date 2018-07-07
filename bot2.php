821c8f9507511e0530417c50c4680b695029a964
watpnas <watpnas@gmail.com>
2018-07-07 23:25:04 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -14,13 +14,15 @@ $options = array(
         'content' => http_build_query($data)
     )
 );
-$context  = stream_context_create($options);
-//$result = file_get_contents($url, false, $context);
-if ($result === FALSE) { /* Handle error */ }
 
-//var_dump($result);
 foreach ($data as $d) {
     echo $d->type; 
-    echo "<br>";
+    echo "<br/>";
 } 
 //echo $content;
+
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { /* Handle error */   echo 'error'; }
+
+var_dump($result);
