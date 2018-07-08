415baa2a552cb79cdd242f40807caddd089e8444
watpnas <watpnas@gmail.com>
2018-07-08 22:21:48 +0700

    Update messenger.php

--- messenger.php
+++ messenger.php
@@ -1,32 +1,25 @@
 <?php
-$url = 'https://api.line.me/v2/bot/message/push';
-$data = '{
-"to":"U260c3c643d920b8e07e0f3d8b887e30a",
-  "messages":[{
-      "type": "template",
-      "altText": "Client's Site 001\n► Device A7 is No Link\n► High Temp Alarm Location A",
-      "template":{
-        "type":"buttons",
-        "text":"Client's Site 001\n► Device A7 is No Link\n► High Temp Alarm Location A",
-        "actions":[{
-          "type":"uri",
-          "label":"View on Device",
-          "uri":"http://xweb-lotus-hyper-30.dyndns.org"
-        }]
-      }
-  }]
-}';
+  $access_token ='Y27E0UjR15NW0tcFH6jy9ku4z+0qLN1uSqTapuVLflT7W7lsOwS/9qZIxIvbBj7rEE0F8H+qZG4QqoWz6fTD7D80Fgamap2na10G36zr0FCGJKi2+HCq9HoLXC2ae77uSzim5PiSOlHXz4caG3U+VwdB04t89/1O/w1cDnyilFU='; 
+  $receiver = "U260c3c643d920b8e07e0f3d8b887e30a";	
 
-// use key 'http' even if you send the request to https://...
-$options = array(
-    'http' => array(
-        'header'  => "Content-type: application/json, Authorization: Bearer Y27E0UjR15NW0tcFH6jy9ku4z+0qLN1uSqTapuVLflT7W7lsOwS/9qZIxIvbBj7rEE0F8H+qZG4QqoWz6fTD7D80Fgamap2na10G36zr0FCGJKi2+HCq9HoLXC2ae77uSzim5PiSOlHXz4caG3U+VwdB04t89/1O/w1cDnyilFU=",
-        'method'  => 'POST',
-        'content' => http_build_query($data)
-    )
-);
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-if ($result === FALSE) { /* Handle error */ }
+$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 
-var_dump($result);
+// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/push';
+			$data = [
+				'to' => $receiver,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
