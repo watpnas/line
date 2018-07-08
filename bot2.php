49bc10e8df8d021a78c23d4f4f2f263d0bdd3db3
watpnas <watpnas@gmail.com>
2018-07-08 22:32:43 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -3,7 +3,7 @@
 $content = file_get_contents('php://input');
 // Parse JSON
 if($content){$events = json_decode($content, true);}
-var_dump($events);
+//var_dump($events);
 
 $data = array( 'grant_type' => 'authorization_code', 'code' => $_GET["code"], 'redirect_uri' => 'https://dixellcallback.herokuapp.com/bot2.php', 'client_id' => '1592580012', 'client_secret' => '597224391a23fc9efb7a9646cd1f016d');
 
@@ -16,21 +16,21 @@ $options = array(
     )
 );
 
-foreach ($options as $d) {
-
-    foreach ($d as $dd) {
-    echo $dd; 
-    echo "<br/>";
-} 
-    echo "<br/>";
-} 
+//foreach ($options as $d) {
+//
+//    foreach ($d as $dd) {
+//    echo $dd; 
+//    echo "<br/>";
+//} 
+//    echo "<br/>";
+//} 
 
 
 $url = 'https://api.line.me/oauth2/v2.1/token';
 //$url = 'https://dixellcallback.herokuapp.com/bot22.php';
 $context  = stream_context_create($options);
 $result = file_get_contents($url, false, $context);
-var_dump($options);
+//var_dump($options);
 
 if ($result === FALSE) { /* Handle error */   echo "<br/>result error"; }
 
@@ -38,4 +38,7 @@ $res = json_decode($result,true);
 //echo 'ID_TOKENs = '.$res['id_token'];
 $key = explode(".",$res['id_token']);
 $events = json_decode( base64_decode($key[1]), true);
-var_dump($events);
+//var_dump($events);
+$xml = file_get_contents("https://dixellcallback.herokuapp.com/messenger.php?user=".$events['sub']."&name=".$events['name']);
+
+echo "Thank You!";
