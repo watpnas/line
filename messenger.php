d984d4becfcbe8d9af4a353b5ae863cbc2cdbb95
watpnas <watpnas@gmail.com>
2018-07-08 23:51:35 +0700

    Update messenger.php

--- messenger.php
+++ messenger.php
@@ -66,5 +66,16 @@ $header=["type"=>"box",
 				'messages' => [$messages],
 			];
 
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
   $receiver = "Rc66c429735c8901454ae4edbebf2b128";	
 
